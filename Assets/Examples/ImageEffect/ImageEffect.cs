e126520ee11700171856c31d70c09428b029fba6
simonne.xu <simonnexupro@gmail.com>
2020-06-01 00:44:58 +0800

    dissolve

---
+++ Assets/Examples/ImageEffect/ImageEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class ImageEffect : MonoBehaviour
+{
+    GComponent _mainView;
+    GObject _testObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainView = this.GetComponent<UIPanel>().ui;
+        _testObj = _mainView.GetChild("n6");
+
+
+        var dissolveFilter = new DissolveFilter();
+        _testObj.filter = dissolveFilter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Extensions/ImageEffect/Dissolve/DissolveFilter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public sealed class DissolveFilter : IFilter
+{
+    [Range(0, 1)]
+    public float threshold = 0.0f;
+
+    Material _mat;
+
+    DisplayObject _target;
+
+    public DisplayObject target
+    {
+        get { return _target; }
+
+        set
+        {
+            _target = value;
+
+            _mat = new Material(ShaderConfig.GetShader("FairyGUIExt/Dissolve"));
+            _target.graphics.material = _mat;
+            // _mat.hideFlags = DisplayObject.hideFlags;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Dispose()
+    {
+        _target = null;
+    }
+
+}
